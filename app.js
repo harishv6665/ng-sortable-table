21c99890fb939f9b7cb05debdc1fc4ea7a9d4dda
harish-rzt <harishv6665@gmail.com>
2017-09-27 01:11:54 +0530

    Ur refactor

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 angular.module('sortApp', [])
 
     .controller('mainController', function($http) {
-        const self = this;
+
+        var self = this;
+
         // Initial values
         self.tableHeadData = [];
         self.tableBodyData = [];
         self.sortType = '';
         self.sortReverse = false;
 
-        //
+        // onSuccess promise
         var onGetTableDataSuccess = function(success) {
             self.tableHeadData = Object.keys(success.data[0]);
             self.tableBodyData = success.data;
             self.sortType = self.tableHeadData[0];
         };
-        
+
+        // onError promise
         var onGetTableDataError =  function(error) {
             alert('Failed to fetch data!' + error);
         };
 
+        // getData
         self.getTableData = function() {
             $http.get('http://jsonplaceholder.typicode.com/posts').then(onGetTableDataSuccess, onGetTableDataError);
-        };
-
-        self.getTableData()
+        }(); // self calling without having to write another like to call the function
 
     });
